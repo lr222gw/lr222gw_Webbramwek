5b20cc5835c08f0493bd15a3c93a5dae8de78aa4
lr222gw <lr222gw@student.lnu.se>
2015-03-19 09:06:37 +0100

    Filtrera på tagg går nu

--- KlientsideProjekt/app/app.js
+++ KlientsideProjekt/app/app.js
@@ -52,10 +52,11 @@ app.directive("allEventsbox", function(){
     return {
         restrict: "E",
         templateUrl : "shared/event/allEventboxTemplate.html",
-        controller : ["$http", "API",function($http, API){
+        controller : ["$http", "API", "$scope",function($http, API, $scope){
             var events = {};
             var activeEvent = {};
             var EventCtrl = this;
+            var tags = []
 
             var promise = $http.get("http://127.0.0.1:3000/api/v1/events?" + API.apikey);
 
@@ -68,9 +69,59 @@ app.directive("allEventsbox", function(){
                 console.log(data);
             });
 
+            var promise2 = $http.get("http://127.0.0.1:3000/api/v1/tags?" + API.apikey);
+
+            promise2.success(function(data){
+                EventCtrl.tags = data.entries;
+                console.log(EventCtrl.tags)
+            });
+
+            promise2.error(function(data){
+                console.log(data);
+            });
+
             this.setActiveEvent = function(event){
                 EventCtrl.activeEvent = event;
             }
+
+            this.setSearch = function(tag){
+                if($scope.query !== undefined && $scope.query[0] === "#"){
+                    $scope.query += ", #"+ tag.name;
+                }else{
+                    $scope.query = "#"+ tag.name;
+                }
+            }
+
+            $scope.search = function (event){
+
+                var name = event.name.toLowerCase();
+                var posName = event.position.name.toLowerCase();
+                var query =  $scope.query !== undefined ? $scope.query.toLowerCase() : "" ;
+                var eventTags = [];
+
+                for(var i = 0; i < event.tag_on_events.length; i++){
+                    eventTags.push("#"+ event.tag_on_events[i].tag.name.toLowerCase())
+                }
+                var arrWithQueryTags = [];
+                var severalQueryTrue = false
+                if($scope.query !== undefined && $scope.query[0] === "#"){ //om det är en tagg vi kollar efter så kan det finnas flera...
+                    arrWithQueryTags = $scope.query.split(", ");
+                    console.log(arrWithQueryTags)
+
+                    for(var i = 0; i < arrWithQueryTags.length; i++){
+                        if(eventTags.indexOf(arrWithQueryTags[i].toLowerCase())!= -1){
+                            severalQueryTrue = true;
+                        }
+                    }
+
+                }
+
+                if (name.search(query)!=-1 || posName.search(query)!=-1 || eventTags.indexOf(query) != -1 || severalQueryTrue) {
+
+                    return true;
+                }
+                return false;
+            };
 
         }],
         controllerAs: "eventCtrl"
