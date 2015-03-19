9952d11dd54dc5ce5d01963c7ec74b9889da6dbd
lr222gw <lr222gw@student.lnu.se>
2015-03-19 02:20:50 +0100

    Fått in kartan

--- KlientsideProjekt/app/app.js
+++ KlientsideProjekt/app/app.js
@@ -1,8 +1,26 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngMap', 'ngRoute']);
+
+app.controller('MapController', function($scope){
+
+    var vm = this;
+
+    var map;
+
+    $scope.$on('mapInitialized', function(evt, evtMap){
+       map = evtMap;
+    });
+
+    vm.fillWithEvents = function(){
+
+    };
+
+
+});
+
 
 app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/view1'});//DIDi
 }]);
